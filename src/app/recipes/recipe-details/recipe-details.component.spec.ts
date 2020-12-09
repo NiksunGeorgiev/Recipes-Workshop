c7312110420e2ddc23c023a23943ccf73cbaaef6
Nikolay Georgiev <georgievny123@gmail.com>
2020-12-09 15:57:50 +0200

    feat:add sections in component and navbar in header

--- src/app/recipes/recipe-details/recipe-details.component.spec.ts
+++ src/app/recipes/recipe-details/recipe-details.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DetailListComponent } from './detail-list.component';
+import { RecipeDetailsComponent } from './recipe-details.component';
 
-describe('DetailListComponent', () => {
-  let component: DetailListComponent;
-  let fixture: ComponentFixture<DetailListComponent>;
+describe('RecipeDetailsComponent', () => {
+  let component: RecipeDetailsComponent;
+  let fixture: ComponentFixture<RecipeDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailListComponent ]
+      declarations: [ RecipeDetailsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailListComponent);
+    fixture = TestBed.createComponent(RecipeDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
